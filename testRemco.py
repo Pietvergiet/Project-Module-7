40ad2fc617ebbf684fb94d8c2e5f23e49ad95076
Remcconen <r.d.brunsveld@student.utwente.nl>
2015-02-18 15:55:06 +0100

    nice and shiny

--- testRemco.py
+++ testRemco.py
@@ -1,12 +1,34 @@
 import graphIO
 import basicgraphs
 
-L = graphIO.loadgraph('week1/crefBM_4_7.grl', readlist=True)
-G=[0 for i in range(len(L[0]))]
+def setColorAsNrNeighbors(graph):
+	for i in range(len(graph.V())):
+		graph[i].colornum = graph[i].deg()
+
+def loadGraphs(file):
+	L = graphIO.loadgraph(file, readlist=True)
+	G=[0 for i in range(len(L[0]))]
+	for i in range(len(G)):
+		G[i] = L[0][i]
+	return G
+
+## MAIN
+
+G=loadGraphs('week1/crefBM_4_7.grl')
+
+for i in range(len(G)):
+	setColorAsNrNeighbors(G[i])
+
+################
+################
+################
+
+
+"""
+colors=[[] for j in range(len(G))]
 
 for i in range(len(G)):
 	G[i] = L[0][i]
-
 # for i in range(len(G)):
 # 	if len(G[0].V()) != len(G[i].V()) or len(G[0].E()) != len(G[i].E()):
 # 		G.pop(i)
@@ -15,15 +37,31 @@ for i in range(len(G)):
 for x in range(len(G)):
 	for i in range(len(G[x].V())):
 		G[x][i].colornum=G[x][i].deg()
+		if G[x][i].deg() not in colors[x]:
+			colors[x]
+			colors[x].append(G[x][i].deg())
+			print(colors[x])
+			colors[x][colors[x].index(G[x][i].deg())] = []
+			print(colors[x])
+			colors[x][colors[x].index(G[x][i].deg())].append(i)
 		# print(G[x][i].colornum)
-colors=[0 for i in range(len(G))]
-for x in range(len(G)):
-	colors[x]=[]
-	for i in range(len(G[x].V())):
-		colors[x].append(G[x][i].colornum)
+sortedcolors=[0 for i in range(len(G))]
 
-for i in range(len(colors)):
-	# colors[i].sort()
-	print(colors[i])
+print(colors)
 
-graphIO.writeDOT(G[0], 'graph.dot')
+"""
+# for x in range(len(G)):
+# 	sortedcolors[x]=[]
+# 	for i in range(len(G[x].V())):
+# 		sortedcolors[x].append(G[x][i].colornum)
+
+# for i in range(len(sortedcolors)):
+# 	# sortedcolors[i].sort()
+# 	print(sortedcolors[i])
+
+# for i in range(len(G)):
+# 	for x in range(len(G[i].V())):
+		
+# 		G[i][x]
+
+# graphIO.writeDOT(G, 'graph.dot')
