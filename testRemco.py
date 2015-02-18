7aa731c0fc8871da9d2293713dd826b7189e1fb3
Remcconen <r.d.brunsveld@student.utwente.nl>
2015-02-18 15:18:11 +0100

    graph.dot

--- testRemco.py
+++ testRemco.py
@@ -7,14 +7,23 @@ G=[0 for i in range(len(L[0]))]
 for i in range(len(G)):
 	G[i] = L[0][i]
 
-for i in range(len(G)):
-	if len(G[0].V()) != len(G[i].V()) or len(G[0].E()) != len(G[i].E()):
-		G.pop(i)
+# for i in range(len(G)):
+# 	if len(G[0].V()) != len(G[i].V()) or len(G[0].E()) != len(G[i].E()):
+# 		G.pop(i)
 		
-print(len(G))
+# print(len(G))
 for x in range(len(G)):
-	G[x] = L[0][x]
-	print("Graph ", x)
 	for i in range(len(G[x].V())):
 		G[x][i].colornum=G[x][i].deg()
-		print(G[x][i].colornum)
+		# print(G[x][i].colornum)
+colors=[0 for i in range(len(G))]
+for x in range(len(G)):
+	colors[x]=[]
+	for i in range(len(G[x].V())):
+		colors[x].append(G[x][i].colornum)
+
+for i in range(len(colors)):
+	# colors[i].sort()
+	print(colors[i])
+
+graphIO.writeDOT(G[0], 'graph.dot')
