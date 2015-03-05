bcdf130821ea4f743c7ec3a2ba6160894598b2b0
Remcconen <r.d.brunsveld@student.utwente.nl>
2015-03-05 12:33:50 +0100

    Added notes

--- testRemco.py
+++ testRemco.py
@@ -40,36 +40,45 @@ def createDisjointUnion(graphs):
 	print("Disjoint created!")
 	return G
 
-def checkIsomorph(graph, colors):
-	graphs = []
-	for i in range(nrOfGraphs):
-		graphs.append([])
+def checkIsomorph(graph):				# return a list with at index 0 graphs with duplicate colors, grouped by the colors, and at the other indices grouped isomorphs					
+	colors=[[] for i in range(nrOfGraphs)]		# creates a list of len nrOfGraphes filled with empty lists
 
-	nodes = graph.V()
+	nodes = graph.V()					# fills the list with the colors, grouped per graph, graph 0 at index 0	
 	for j in range(nrOfGraphs):
 		for i in range(nrOfNodes):
-			# print("i", (i+(j*nrOfNodes)))
-			graphs[j].append(nodes[(i+(j*nrOfNodes))].colornum)
-			# print((i+(j*nrOfNodes)))
+			colors[j].append(nodes[(i+(j*nrOfNodes))].colornum)
 
-	for i in range(len(graphs)):
-		merge_sort(graphs[i])
-		# print(i, ":", graphs[i])
+	for i in range(len(colors)):		# sorts the lists with colors from low to high
+		merge_sort(colors[i])
+		# print(i, ":", colors[i])
 
-	isomorphs = [[]]
-	for i in range(len(graphs)):
-		if len(graphs[i]) != len(set(graphs[i])):
-			isomorphs[0].append(i)
-		else:
+	isomorphs = [[]]					# list for the (possible) isomorphs
+
+	for i in range(len(colors)):						# loop through all lists with the colors of the graph
+		if len(colors[i]) != len(set(colors[i])):		# Check for duplicates. A set can not contain duplicates
 			added = False
-			for j in range(1, len(isomorphs)):
-				# print(j, i, graphs[isomorphs[j][0]], graphs[i])
-				if(graphs[isomorphs[j][0]] == graphs[i]):
-					isomorphs[j].append(i)
+			for j in range(len(isomorphs[0])):			# loop through all possible pairs of isomorphs, but colors contain duplicates
+				# print(i, j, len(isomorphs[0][0]))
+				if len(isomorphs[0][j]) != 0 and colors[i] == colors[isomorphs[0][j][0]]:		# If a graph is added and colors of the graph i are the same as the colors of the graph already added
+					isomorphs[0][j].append(i)			# add the graph to the pair of possible isomorphs
 					added = True
-			if not added:
+			if not added:								# add graph at new index if no matching colors are found
+				isomorphs[0].append([i])
+		else:											# no duplicates found
+			added = False
+			for j in range(1, len(isomorphs)):			# loop through all pairs of isomorphs
+				if(colors[isomorphs[j][0]] == colors[i]):	# If colors of graph i are the same as the colors of the graph already added
+					isomorphs[j].append(i)				# group the graph with his isomorphs
+					added = True
+			if not added:								# add graph at new index if no matching colors are found
 				isomorphs.append([i])
 
+	printIsomorphs(isomorphs)
+
+	return isomorphs
+
+# prints the graphs, grouped by isomorphs
+def printIsomorphs(isomorphs):
 	if len(isomorphs[0]) != 0:
 		print("Unable to check: ", isomorphs[0])
 	for i in range(1, len(isomorphs)):
@@ -77,11 +86,6 @@ def checkIsomorph(graph, colors):
 			print("Not isomorph: ", isomorphs[i])
 		else:
 			print("Isomorph: ", isomorphs[i])
-
-
-
-
-
 
 
 
@@ -122,72 +126,36 @@ def getNeighborsColors(node):
 # Gaat verder kleuren toekennen aan de graven tot het niet meer mogelijk is.
 def colorRefinement(graph, colors):
 	rColors = copy.copy(colors)
-	for i in range(len(rColors)):				# KLEUR
-	# for i in range(0, 2):
-		nbss = len(rColors[i][0].nbs())
-		# print("amount of nbs:", nbss)
-		nodes=copy.copy(rColors[i])
+	for i in range(len(rColors)):				# loop through all the colors
+		nodes=copy.copy(rColors[i])				# create a copy of the nodes
 		first = True
-		while(len(nodes) != 0):
-			if not first:
-				rColors[nodes[0].colornum].remove(nodes[0])
-				nodes[0].colornum = len(rColors)
-				rColors.append([nodes[0]])
+
+		while(len(nodes) != 0):					# while nodes have to be recolored
+			if not first:						# if not the first run ----- in the first run the first node doesn't need a new color. After the first run, it does. First node has to be a unique color
+				rColors[nodes[0].colornum].remove(nodes[0])			# remove first node
+				nodes[0].colornum = len(rColors)					# give first node new color
+				rColors.append([nodes[0]])							# add first node to new color index
 			first = False
-			checkednodes=[]
-			# print("nodes", nodes)
-			for q in range(1, len(nodes)): 		# nodes
-				if getNeighborsColors(nodes[0]) == getNeighborsColors(nodes[q]):				# color neighbors 1 = color neighbors q
-					rColors[nodes[q].colornum].remove(nodes[q])
-					nodes[q].colornum = nodes[0].colornum
-					rColors[nodes[0].colornum].append(nodes[q])
-					# print("rColors append, ", nodes[0].colornum, nodes[q], "q: ", q)
-					checkednodes.append(nodes[q])
-					# nodes.remove(nodes[q])
-				# else:
-				# 	print("rCOlors", rColors)
-				# 	print("remove from", nodes[q].colornum, nodes[q])
-				# 	rColors[nodes[q].colornum].remove(nodes[q])
-				# 	nodes[q].colornum = len(rColors)
-				# 	rColors.append([nodes[q]])
-				# 	# print("rColors append new, ", nodes[q], "q: ", q)
+			colorednodes=[nodes[0]]									# node 0 is always recolored
 
-			for i in range(len(checkednodes)):
-				nodes.remove(checkednodes[i])
-			nodes.remove(nodes[0])
+			for q in range(1, len(nodes)): 									# loop through all the nodes that have to be recolored
+				if getNeighborsColors(nodes[0]) == getNeighborsColors(nodes[q]):	# if colors of the neighbors of 0 and q are the same
+					rColors[nodes[q].colornum].remove(nodes[q])				# remove node q from old color index
+					nodes[q].colornum = nodes[0].colornum					# give node q same color as node 0
+					rColors[nodes[0].colornum].append(nodes[q])				# add node q to new color index
+					colorednodes.append(nodes[q])							# add node q to list with recolored nodes
 
-			while [] in rColors:
+			for i in range(len(colorednodes)):			# loop through all recolored nodes
+				nodes.remove(colorednodes[i])			# remove recolored nodes from nodes that need to be recolored
+
+			while [] in rColors:			# remove empty lists in the colorlist
 				rColors.remove([])
 			
-	if(rColors != colors):
+	if(rColors != colors):					# while the colors have changed in this function, do colorRefinement again
 		colorRefinement(graph, rColors)
 	return rColors
 
-
-
-
-
-
-			# print(q)
-			# x = 1
-			# while x <= nbss:
-			# 	# print(x, nbss)
-			# 	for j in range(x, 0, -1):
-			# 		for k in range(nbss):
-			# 			same = False
-			# 			for l in range(nbss):
-			# 				if rColors[i][x].nbs()[k].colornum == rColors[i][j].nbs()[l].colornum or same:
-			# 					same = True
-			# 			if not same :
-			# 				print(q, j, k, l)
-			# 				rColors[i][j].colornum=len(rColors)
-			# 				colors.append([rColors[i][j]])
-			# 				break
-			# 	x+=1			
-
-
-	# return rColors
-
+# load the graphs into a list
 def loadGraphs(file):
 	L = graphIO.loadgraph(file, readlist=True)
 	G=[0 for i in range(len(L[0]))]
@@ -195,6 +163,7 @@ def loadGraphs(file):
 		G[i] = L[0][i]
 	return G
 
+#sorts a list
 def merge_sort(alist):
     if len(alist)>1:
         mid = len(alist)//2
@@ -229,69 +198,23 @@ def merge_sort(alist):
 
 ## MAIN
 
-G=loadGraphs('week1/crefBM_2_49.grl')
+G=loadGraphs('week2/trees11.grl')
 #print("aantal graphs: ", len(G))-
 H = createDisjointUnion(G)
 nbs = H.V()[0].nbs()
 #print(H[nbs[0]._label])
-graphIO.writeDOT(G[0], 'graph1.dot')
-graphIO.writeDOT(G[1], 'graph2.dot')
+# graphIO.writeDOT(G[0], 'graph1.dot')
+# graphIO.writeDOT(G[1], 'graph2.dot')
 # graphIO.writeDOT(G[2], 'graph3.dot')
 # graphIO.writeDOT(G[3], 'graph4.dot')
-# graphIO.writeDOT(H, 'graph.dot')
+graphIO.writeDOT(H, 'graph.dot')
 # for i in range(len(G)):
 colors = setColorAsNrNeighbors(H)
 # print("Colors: ", colors)
 colors = colorRefinement(H, colors)
 # print("rColors: ", colors)
 
-checkIsomorph(H, colors)
+checkIsomorph(H)
 
 #print("Colors: \n",colors)
 graphIO.writeDOT(H, 'graph_colors.dot')
-################
-################
-################
-
-
-"""
-colors=[[] for j in range(len(G))]
-
-for i in range(len(G)):
-	G[i] = L[0][i]
-# for i in range(len(G)):
-# 	if len(G[0].V()) != len(G[i].V()) or len(G[0].E()) != len(G[i].E()):
-# 		G.pop(i)
-		
-# print(len(G))
-for x in range(len(G)):
-	for i in range(len(G[x].V())):
-		G[x][i].colornum=G[x][i].deg()
-		if G[x][i].deg() not in colors[x]:
-			colors[x]
-			colors[x].append(G[x][i].deg())
-			print(colors[x])
-			colors[x][colors[x].index(G[x][i].deg())] = []
-			print(colors[x])
-			colors[x][colors[x].index(G[x][i].deg())].append(i)
-		# print(G[x][i].colornum)
-sortedcolors=[0 for i in range(len(G))]
-
-print(colors)
-
-"""
-# for x in range(len(G)):
-# 	sortedcolors[x]=[]
-# 	for i in range(len(G[x].V())):
-# 		sortedcolors[x].append(G[x][i].colornum)
-
-# for i in range(len(sortedcolors)):
-# 	# sortedcolors[i].sort()
-# 	print(sortedcolors[i])
-
-# for i in range(len(G)):
-# 	for x in range(len(G[i].V())):
-		
-# 		G[i][x]
-
-# graphIO.writeDOT(G, 'graph.dot')
