9c26a937ef35612f1a6787d84cb3c36f767b8377
joerikock <joerikock@live.nl>
2015-03-18 11:16:09 +0100

    YOLO!!!!!

--- preprocessing.py
+++ preprocessing.py
@@ -3,3 +3,48 @@ import basicgraphs
 import copy
 import operator
 
+def createDisjointUnion(graphs):
+	G=basicgraphs.graph()
+	global nrOfGraphs
+	nrOfGraphs = len(graphs)
+	global nrOfNodes
+	nrOfNodes = len(graphs[0].V())
+	# print(nrOfGraphs, nrOfNodes)
+	for i in range(len(graphs)):
+	# for i in range(0, 1):
+		# print("Graph ", i)
+		index = len(G.V())
+		for j in range(len(graphs[i].V())):
+			G.addvertex(graphs[i].V()[j])		# Gooi de vertices van graaf i in de graaf
+			G[j+index].newLabel(j+index)
+
+		for j in range(len(graphs[i].V())):
+			#print("Graph: ", i, "Vertex: ", j)
+			nbs = graphs[i].V()[j].nbs()	# alle neighbours van vertex j in graaf i
+			# print("NBS: ", j, nbs)
+			for x in range(len(nbs)):
+				# print(x)
+				try: 														# maakt dezlefde egdes met tussen de vertex en zijn neighbours als in de originele graaf
+					G.addedge(G[j+index], G[nbs[x]._label+index])
+					# print("Edge from ", j, "to ", nbs[x]._label)	
+					
+				except basicgraphs.GraphError:
+					pass
+					#print("Egde is dubbel want logica* \n *previous statement not to be taken sarcastic")		# elke neigbour komt 2 keer voor dus hiermee worden de dubbele gevallen afgevangen
+	#print(G)
+	print("Disjoint created!")
+	return G
+
+def loadGraphs(file):
+	L = graphIO.loadgraph(file, readlist=True)
+	G=[0 for i in range(len(L[0]))]
+	for i in range(len(G)):
+		G[i] = L[0][i]
+	return G
+
+def preCheck(graph):
+	pass
+
+G = loadGraphs('week1/crefBM_2_49.grl')
+H = createDisjointUnion(G)
+graphIO.writeDOT(H, 'graph.dot')
